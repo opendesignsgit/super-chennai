9936cbcd9248f864161436c9c477bc3a198c385a
Bharathi <bharathidhasanodi@gmail.com>
2025-06-26 13:38:31 +0530

    add descktop electran

---
+++ electron.js
@@ -0,0 +1,41 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    show: true, // required for `ready-to-show` to work
+    icon: path.join(__dirname, 'public', 'favicon.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  win.loadURL('http://localhost:3000')
+  win.webContents.openDevTools()
+
+  win.once('ready-to-show', () => {
+    win.show()
+    win.focus()
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
