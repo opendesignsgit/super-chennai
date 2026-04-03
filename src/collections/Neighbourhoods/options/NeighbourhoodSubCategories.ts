440d56fbf21eb227211a2c1d177edf3f81faaa4b
Bharathi <bharathi0307@gmail.com>
2026-04-03 16:23:56 +0530

    bug fx

--- src/collections/Neighbourhoods/options/NeighbourhoodSubCategories.ts
+++ src/collections/Neighbourhoods/options/NeighbourhoodSubCategories.ts
@@ -2,9 +2,27 @@ import { CollectionConfig } from 'payload'
 
 export const NeighbourhoodSubCategories: CollectionConfig = {
   slug: 'neighbourhood-subcategories',
+
   admin: { useAsTitle: 'title' },
+
+  access: {
+    read: () => true,
+  },
+
   fields: [
-    { name: 'title', type: 'text', required: true },
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+
+    {
+      name: 'slug',
+      type: 'text',
+      unique: true,
+      required: true,
+    },
+
     {
       name: 'parentCategory',
       type: 'relationship',
