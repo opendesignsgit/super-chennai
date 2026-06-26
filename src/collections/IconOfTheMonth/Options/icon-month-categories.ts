c7150d7648f375f49196be9d99604bf864f4790a
Bharathi <bharathi0307@gmail.com>
2026-06-26 17:59:07 +0530

    add update css

--- src/collections/IconOfTheMonth/Options/icon-month-categories.ts
+++ src/collections/IconOfTheMonth/Options/icon-month-categories.ts
@@ -3,36 +3,36 @@ import type { CollectionConfig } from 'payload'
 export const IconMonthCategories: CollectionConfig = {
   slug: 'icon-month-categories',
   admin: {
-    // useAsTitle: 'title',
+    useAsTitle: 'title',
     group: 'Settings',
   },
   fields: [
-    // {
-    //   name: 'title',
-    //   type: 'text',
-    //   required: true,
-    // },
-    // {
-    //   name: 'slug',
-    //   type: 'text',
-    //   required: true,
-    //   unique: true,
-    // },
-    // {
-    //   name: 'description',
-    //   type: 'textarea',
-    // },
-    // {
-    //   name: 'icon',
-    //   type: 'upload',
-    //   relationTo: 'media',
-    // },
-    // {
-    //   name: 'color',
-    //   type: 'text',
-    //   admin: {
-    //     description: 'Hex color (#FF5722)',
-    //   },
-    // },
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'icon',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'color',
+      type: 'text',
+      admin: {
+        description: 'Hex color (#FF5722)',
+      },
+    },
   ],
 }
